212e2acc9cde8a70ee08076ed9c0605bf0fdc9c6
Chris Wong <chriswong@syn-174-104-040-193.res.spectrum.com>
2024-06-11 14:46:02 +0800

    add language allowlist

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function geoip(json){
 
 const userLang = navigator.language || navigator.userLanguage
 console.log('userLang', userLang)
+
+const LANGUAGE_ALLOWLIST = ['ar-ae','ar-eg']
+
+if (LANGUAGE_ALLOWLIST.includes(userLang)) {
+  console.log('yup')
+} else {
+  console.log('nah')
+}
