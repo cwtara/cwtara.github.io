92778bb97c18e07e21253e0d698b6e6c1d8e229c
Chris Wong <chriswong@syn-174-104-040-193.res.spectrum.com>
2024-06-11 15:21:26 +0800

    add window onload

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 console.log('Current time: ', Date.now())
 
-function geoip(json){
-  console.log('geoip', json)
-}
+// function geoip(json){
+//   console.log('geoip', json)
+// }
 
+/** start locale handling */
 const userLang = navigator.language || navigator.userLanguage
 console.log('userLang', userLang)
 
 const LANGUAGE_ALLOWLIST = ['ar-ae','ar-eg']
 
 if (LANGUAGE_ALLOWLIST.includes(userLang.toLowerCase())) {
-  console.log('yup')
-  window.location.replace(`/${userLang}`)
+  console.log('language redirect')
+  // window.location.replace(`/${userLang}`)
 } else {
-  console.log('nah')
+  console.log('do not redirect')
+}
+
+/** end locale handling */
+
+/** start currency handling */
+window.onload = () => {
+  const geoip = (json) => {
+    console.log('geoip', json)
+  }
 }
