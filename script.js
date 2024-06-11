a2deac9397707de9a1389e09525e86a130ee7c7c
Chris Wong <chriswong@syn-174-104-040-193.res.spectrum.com>
2024-06-11 14:05:10 +0800

    update index and script

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Test', Date.now())
+
+function geoip(json){
+  var userip      = document.getElementById("user_ip");
+  var countrycode = document.getElementById("user_countrycode");
+  userip.textContent      = json.ip;
+  countrycode.textContent = json.country_code;
+}
