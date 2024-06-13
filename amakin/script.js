8b208b6cb18e9def556721cbe39d687b53d72092
Chris Wong <chriswong@syn-174-104-040-254.res.spectrum.com>
2024-06-13 13:45:13 +0800

    use getCurrency method from shopyflow

--- amakin/script.js
+++ amakin/script.js
@@ -20,7 +20,7 @@ if (LANGUAGE_REDIRECT_ALLOWLIST.includes(userLang.toLowerCase())) {
 window.onload = () => {
   console.log('geoIPData', geoIPData)
 
-  const selectedCurrency = typeof Shopyflow !== 'undefined' && Shopyflow.config?.storefrontConfig?.storeCurrency.toLowerCase() || ''
+  const selectedCurrency = typeof Shopyflow !== 'undefined' && Shopyflow.getCurrency().toLowerCase() || ''
   const userCountry = geoIPData.country_code.toLowerCase()
   const isCurrencyMatchUserCountryCode = selectedCurrency == userCountry
   console.log('selectedCurrency', selectedCurrency)
@@ -30,6 +30,7 @@ window.onload = () => {
   if (isCurrencyMatchUserCountryCode) {
     // hide currency select modal (or do nothing if we are auto-setting)
   } else if (typeof Shopyflow !== 'undefined') {
+    // automatically set currency in Shopyflow
     Shopyflow.setCurrency({ countryCode: userCountry })
   } else {
     // show currency select modal
