c07a879b4d6e50bf4688cf5d1a089d6dcf9f2fb3
Chris Wong <chriswong@Chriss-MacBook-Pro.local>
2024-07-11 12:38:32 +0700

    add listener

--- anatomy/script.js
+++ anatomy/script.js
@@ -1,4 +1,8 @@
 window.onload = () => {
   const widgetElement = document.querySelector('[data-oke-widget]')
   if (window.okeWidgetApi) window.okeWidgetApi.setProduct(widgetElement, 'shopify-8044971000130')
+
+  document.addEventListener('oke-analytics-event', e => {
+    console.log('event', e)
+  })
 }
