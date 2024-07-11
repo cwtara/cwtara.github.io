f55b8cb9f250684686401068f10b3d4faa54bde9
Chris Wong <chriswong@Chriss-MacBook-Pro.local>
2024-07-11 13:33:50 +0700

    test

--- anatomy/script.js
+++ anatomy/script.js
@@ -1,5 +1,6 @@
 const SHOPIFY_PRODUCT_ID = 'shopify-8044971000130'
 const OKENDO_SUBSCRIBER_ID = 'ac615854-e743-4537-85c5-b3cbfcb7dcd7'
+let TEST 
 
 window.onload = () => {
   const widgetElement = document.querySelector('[data-oke-widget]')
@@ -7,7 +8,8 @@ window.onload = () => {
     window.okeWidgetApi.setProduct(widgetElement, SHOPIFY_PRODUCT_ID)
       .finally(() => {
         const starElement = $('.oke-w-ratingAverageModule-rating-stars')[0]?.cloneNode(true)
-        console.log('starElement', starElement)
+        TEST = starElement
+        console.log('starElement', starElement, TEST)
       })
   }
 
