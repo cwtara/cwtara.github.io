e57ce04b65d85d40155ab71ac13a60c198851a35
Chris Wong <chriswong@Chriss-MacBook-Pro.local>
2024-07-11 13:30:29 +0700

    add finally

--- anatomy/script.js
+++ anatomy/script.js
@@ -3,11 +3,12 @@ const OKENDO_SUBSCRIBER_ID = 'ac615854-e743-4537-85c5-b3cbfcb7dcd7'
 
 window.onload = () => {
   const widgetElement = document.querySelector('[data-oke-widget]')
-  if (window.okeWidgetApi) window.okeWidgetApi.setProduct(widgetElement, SHOPIFY_PRODUCT_ID)
+  if (window.okeWidgetApi) {
+    window.okeWidgetApi.setProduct(widgetElement, SHOPIFY_PRODUCT_ID)
+      .finally(e => {
+        console.log('e', e)
+      })
+  }
 
   // const starElement = $('.oke-w-ratingAverageModule-rating-stars')[0]?.cloneNode(true)
-
-  document.addEventListener('readystatechange', e => {
-    console.log('e', e)
-  })
 }
