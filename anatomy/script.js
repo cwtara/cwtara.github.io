922be11b5c26f0f045f5d53350d09c3b91b6d4b2
Chris Wong <chriswong@Chriss-MacBook-Pro.local>
2024-07-11 16:47:38 +0700

    set scrolljack to body

--- anatomy/script.js
+++ anatomy/script.js
@@ -49,11 +49,11 @@ window.onload = () => {
   //     $('.product-container').addClass('no-scroll-snap')
   //   }
   // })
-  $('.product-container').on('scroll', () => {
-    if ($('.product-container').scrollTop() >= $('body').innerHeight()) {
-      $('.product-container').addClass('no-scroll-snap')
+  $('body').on('scroll', () => {
+    if ($('body').scrollTop() >= $('body').innerHeight()) {
+      $('body').addClass('no-scroll-snap')
     } else {
-      $('.product-container').removeClass('no-scroll-snap')
+      $('body').removeClass('no-scroll-snap')
     }
   });
 }
