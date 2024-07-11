0fbbb7e48734799e5dcd4b721b3c63f4c9cfacf6
Chris Wong <chriswong@Chriss-MacBook-Pro.local>
2024-07-11 13:51:01 +0700

    update fetch call

--- anatomy/script.js
+++ anatomy/script.js
@@ -1,16 +1,11 @@
 const SHOPIFY_PRODUCT_ID = 'shopify-8044971000130'
 const OKENDO_SUBSCRIBER_ID = 'ac615854-e743-4537-85c5-b3cbfcb7dcd7'
-let TEST 
 
 window.onload = () => {
   const widgetElement = document.querySelector('[data-oke-widget]')
-  if (window.okeWidgetApi) {
-    window.okeWidgetApi.setProduct(widgetElement, SHOPIFY_PRODUCT_ID)
-      .finally(() => {
-        const starElement = $('.oke-w-ratingAverageModule-rating-stars')[0]?.cloneNode(true)
-        TEST = starElement
-        console.log('starElement', starElement, TEST)
-      })
-  }
+  if (window.okeWidgetApi) window.okeWidgetApi.setProduct(widgetElement, SHOPIFY_PRODUCT_ID)
 
+  fetch(`https://api.okendo.io/v1/stores/${OKENDO_SUBSCRIBER_ID}/products/${SHOPIFY_PRODUCT_ID}/review_aggregate`)
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
