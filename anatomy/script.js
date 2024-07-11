db491d69c905f17bbd4c052a143bca12342268e6
Chris Wong <chriswong@Chriss-MacBook-Pro.local>
2024-07-11 15:31:46 +0700

    comment

--- anatomy/script.js
+++ anatomy/script.js
@@ -41,7 +41,7 @@ window.onload = () => {
     });
   
   $('.product-container').on('scroll', () => {
-    console.log('scroll')
+    console.log('scroll', $('.product-container').scrollTop(), $('body').innerHeight())
     if ($('.product-container').scrollTop() > $('body').innerHeight()) {
       console.log('UNSET')
       $('.product-container').css('overflow', 'unset');
